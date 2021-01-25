d8f337d13f4d6cde4a7e8f28ebf609159f14540b
Marshall Miller <contact@broken-titan.com>
2021-01-25 01:38:39 -0500

    Initial commit.

---
+++ src/Providers/PowerBIServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+    namespace BrokenTitan\PowerBI\Providers;
+
+    use Illuminate\Support\ServiceProvider;
+
+    class PowerBIServiceProvider extends ServiceProvider {
+        /**
+         * @method boot
+         * @return void
+         */
+        public function boot() {
+            
+        }
+    }
